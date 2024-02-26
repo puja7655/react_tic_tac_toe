6962b342153eb794a8033f6f579356778985d20f
Puja Singh <puja@Pujas-MacBook-Air.local>
2024-02-26 12:55:26 +0100

    created Players board

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 
 function App() {
-  
+
 
   return (
-    <h1>Coming soon...</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+            </span>
+            <button>Edit</button>
+          </li>
+          <li>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">0</span>
+            </span>
+            <button>Edit</button>
+          </li>        
+        </ol>
+        game board
+      </div>
+    </main >
   )
 }
 
