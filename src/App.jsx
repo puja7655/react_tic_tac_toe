af1ccbac8ce0a4d23352a37cd0e92d83c72158ce
Puja Singh <puja@Pujas-MacBook-Air.local>
2024-02-28 19:10:10 +0100

    code optimization

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import Log from "./components/Log"
 import { WINNING_COMBINATIONS } from "./Winning-combinations"
 import GameOver from "./components/GameOver"
 
-const initialGameBoard = [
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2'
+}
+
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
@@ -19,19 +24,9 @@ function deriveActivePlayer(gameTurns) {
   return currentplayer
 }
 
-function App() {
-
-  const [gameTurns, setGameTurns] = useState([]) //gameTurns is array full of object whose intitial value is set to 0
-  const [players, setPlayers] = useState({
-    'X': 'Player 1',
-    'O': 'Player2'
-  })
-  //const [activePlayer, setActivePlayer] = useState('X')
-  //Here I am removing the above state and trying to derive the activePlayer from gameTurns because we need the activePlayer from updateTurn
-  const activePlayer = deriveActivePlayer(gameTurns)
-
-  // It is not advisable to mutate the original array in javascript
-  let gameBoard = [...initialGameBoard.map(array => [...array])];
+function deriveGameBoard(gameTurns) {
+  // It is not advisable to mutate the original array in javascript so i am making a deep copy of that
+  let gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])];
   //here we are traversing through the turns which is array of object then destructuring it .
   // i.e deriving the values to update the gameboard
   for (const turn of gameTurns) {
@@ -39,7 +34,10 @@ function App() {
     const { row, col } = square;
     gameBoard[row][col] = player
   }
+  return gameBoard;
+}
 
+function deriveWinner(gameBoard, players) {
   let winner
   for (const combination of WINNING_COMBINATIONS) {
     const firstSquareSymboal = gameBoard[combination[0].row][combination[0].col]
@@ -52,19 +50,32 @@ function App() {
       winner = players[firstSquareSymboal];
     }
   }
+  return winner;
+}
+
+function App() {
+
+  const [gameTurns, setGameTurns] = useState([]) //gameTurns is array full of object whose intitial value is set to 0
+  const [players, setPlayers] = useState(PLAYERS)
+  //const [activePlayer, setActivePlayer] = useState('X')
+  //Here I am removing the above state and trying to derive the activePlayer from gameTurns because we need the activePlayer from updateTurn
+  const activePlayer = deriveActivePlayer(gameTurns)
+
+  let gameBoard = deriveGameBoard(gameTurns)
+  let winner = deriveWinner(gameBoard, players)
+
   const hasDraw = gameTurns.length === 9 && !winner
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
 
 
-      //not setting player: activePlayer because that is from different state and hence we will not alwayes get the updated turns or latest turn
-      //PrevTurn is array of object . we are taking the object at first place as that is the latest turn
-
-      // let currentPlayer = 'X'
-      // if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-      //   currentPlayer = 'O'
-      // }
+    /** not setting player: activePlayer because that is from different state and hence we will not alwayes get the updated turns or latest turn
+      * PrevTurn is array of object . we are taking the object at first place as that is the latest turn
+      * let currentPlayer = 'X'
+      * if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+      * currentPlayer = 'O'}
+     */
 
       //here i am deriving the current player and to minimize the duplication of code i have created a helper function
       let currentPlayer = deriveActivePlayer(prevTurns)
@@ -102,12 +113,12 @@ function App() {
       <div id="game-container">
         <ol id="players" className="highlight-player">
           <Player
-            initialName="Player 1"
+            initialName={PLAYERS.X}
             symbol="X"
             isActive={activePlayer === 'X'}
             onNameChange={handlePlayerNameChange} />
           <Player
-            initialName="Player 2"
+            initialName={PLAYERS.O}
             symbol="O"
             isActive={activePlayer === 'O'}
             onNameChange={handlePlayerNameChange} />
